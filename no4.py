79b7120756261fb43f76dd110157d8344e770757
Ahmad Luthfi Mahar <53337165+whitebeard03@users.noreply.github.com>
2019-09-07 21:47:12 +0700

    Update no4.py

--- no4.py
+++ no4.py
@@ -6,6 +6,16 @@ Created on Sat Sep  7 11:13:10 2019
 """
 
 def nomor_4(inp):
+    def check_arranged(_to_arrange):
+        to_arrange = _to_arrange.split("-")
+        if len(to_arrange[-1]) == 1:
+            to_complete = to_arrange[-2][-1]
+            to_complete += to_arrange[-1]
+            to_arrange[-1] = to_complete
+            to_arrange[-2] = to_arrange[-2][:-1]
+            _to_arrange = "-".join(to_arrange)
+        return _to_arrange
+    inp = str(inp)
     arranged = ""
     i = 0
     for seq in inp.split(" "):
@@ -16,6 +26,7 @@ def nomor_4(inp):
                 if i == 3:
                     arranged += "-"
                     i = 0
+    arranged = check_arranged(arranged)
     return arranged
 print(nomor_4("993141 -1 1323 14-232"))
 
