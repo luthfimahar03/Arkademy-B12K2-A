9cf52a878082a3ee8a7732a6046e0c551b3e86fc
Ahmad Luthfi Mahar <53337165+whitebeard03@users.noreply.github.com>
2019-09-07 11:47:01 +0700

    Add files via upload

---
+++ no5.py
@@ -0,0 +1,14 @@
+
+str = "hmm.."
+ 
+jumvokal = 0
+
+
+for i in str:
+	if( i=='A' or i=='a' or i=='E' or i=='e'
+	or i=='I' or i=='i' or i=='O' or i=='o'
+	or i=='U' or i=='u'):
+		jumvokal +=1;
+		
+print ("Jumlah huruf vokal ada: ", jumvokal)
+    
